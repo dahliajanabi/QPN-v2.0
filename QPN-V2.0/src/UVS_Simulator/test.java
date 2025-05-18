38518dafea4eaf503d68e221f15b795a1c50f99c
dahliajanabi@gmail.com <dahliajanabi@gmail.com>
2025-05-18 18:30:05 +0300

    The test class

--- QPN-V2.0/src/UVS_Simulator/test.java
+++ QPN-V2.0/src/UVS_Simulator/test.java
@@ -5,7 +5,7 @@ import Components.PetriNetWindow;
 
 public class test {
 	public static void main(String[] args) {
-
+// 
 		// test 1 Throughput = 2.398308038711548
 //		Gammas[] g = { new Gammas(0.1, 0.2), new Gammas(0.2, 0.3), new Gammas(0.3, 0.4), new Gammas(0.4, 0.5) };
 //		UVS uvs = new UVS(g, Constants.U1, Constants.x_init);
@@ -18,7 +18,7 @@ public class test {
 ////		//pn.Start();
 
 
-		// test 2 Throughput = 2.2627415657043457
+		// test 2 Throughput = 2.4928812384605408
 
 		Gammas[] g2 = { new Gammas(0.2, 0.7), new Gammas(0.1, 0.5), new Gammas(0.6, 0.7), new Gammas(0.8, 0.5) };
 		UVS uvs2 = new UVS(g2, Constants.U1, Constants.x_init);
